ed785720f6ae5fc3ce9386df020ead382441e304
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-15 14:02:24 -0400

    all

--- main.js
+++ main.js
@@ -36,15 +36,33 @@ function onClick() {
     //set its .innerText to the result of passing the input to encode, translate, madlib, search, or random
     if (encodeRadio.checked) {
         resultOutput.innerText = encode(inputContent);
-       //console.log(encode(inputContent, "encode"));
+        //console.log(encode(inputContent, "encode"));
 
     } else if (translateRadio.checked) {
         resultOutput.innerText = translate(inputContent);
         //console.log(translate(inputContent, "translate"))
     } else if (madlibRadio.checked) {
         resultOutput.innerText = madlib(inputContent);
-    } else if (searchRadio.checked){
-        console.log(search())
+    } else if (searchRadio.checked) {
+        //Clear the current content of our <h1 id="results"> output (hint: use .innerHTML)
+        function clearText() {
+            resultOutput.innerText = null
+            //Loop through each object in the array returned by the search() function. For each emoji object in the array:
+            for (let i = 0; i < emojis.length; ++i) {
+                const selectedEmoji = emojis[i];
+
+                const symbolEmoji = selectedEmoji.symbol;
+
+                
+                //if(textInput.value === selectedEmoji){
+                //  resultOutput.innerText = search(emojiSymbol);
+                //}
+            }
+
+        }
+        clearText()
+        //resultOutput.innerHTML( inputContent);
+        console.log(search(inputContent))
     }
 
 
